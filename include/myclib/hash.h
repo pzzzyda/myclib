ef7679071d7eb6e4e7968bc58c0f0f7018e330b9
pzzzyda <zhongyuan03@outlook.com>
2025-10-25 16:45:01 +0800

    feat: add MC_HASH macro
    This commit adds the MC_HASH macro, which is a wrapper of the
    mc_hash_fnv1a64 and mc_hash_fnv1a32 functions depending on the
    architecture.

--- include/myclib/hash.h
+++ include/myclib/hash.h
@@ -3,8 +3,17 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include "myclib/macros.h"
 
 uint64_t mc_hash_fnv1a64(const void *data, size_t len);
 uint32_t mc_hash_fnv1a32(const void *data, size_t len);
+
+#if defined(MC_ARCH_64)
+#define MC_HASH(data, len) mc_hash_fnv1a64(data, len)
+#elif defined(MC_ARCH_32)
+#define MC_HASH(data, len) mc_hash_fnv1a32(data, len)
+#else
+#error "Unknown architecture!"
+#endif
 
 #endif

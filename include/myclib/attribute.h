f82959ffedfd351ab919c4c506bd5614b1fd31b4
pzzzyda <zhongyuan03@outlook.com>
2025-11-15 11:12:03 +0800

    feat: delete some macros that represent specific attributes

--- include/myclib/attribute.h
+++ include/myclib/attribute.h
@@ -5,18 +5,11 @@
 #define MC_COMPILER_SUPPORTS_ATTRIBUTE 1
 
 #define MC_ATTRIBUTE(attr) __attribute__((attr))
-#define MC_ATTR_CONSTRUCTOR __attribute__((constructor))
-#define MC_ATTR_DESTRUCTOR __attribute__((destructor))
-#define MC_ATTR_PRINTF(fmt_idx, args_idx)                                      \
-    __attribute__((format(printf, fmt_idx, args_idx)))
 
 #else
 #define MC_COMPILER_SUPPORTS_ATTRIBUTE 0
 
 #define MC_ATTRIBUTE(attr)
-#define MC_ATTR_CONSTRUCTOR
-#define MC_ATTR_DESTRUCTOR
-#define MC_ATTR_PRINTF(fmt_idx, args_idx)
 
 #endif
 
